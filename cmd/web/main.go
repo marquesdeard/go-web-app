a5af72ae6627979962fb53f037f9d1849071873c
marquesdeard <ekemaldemirel@gmail.com>
2024-02-02 07:54:47 +0300

    Initial Commit

---
+++ cmd/web/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/marquesdeard/web-app/pkg/config"
+	"github.com/marquesdeard/web-app/pkg/handlers"
+	"github.com/marquesdeard/web-app/pkg/render"
+)
+
+var port = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// Session setup
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+
+	app.Session = session
+
+	// in production make it true
+	session.Cookie.Secure = app.InProduction
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal(("cannot create template cache"))
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println(fmt.Sprintf("Starting Application on port %s", port))
+	// _ = http.ListenAndServe(port, nil)
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
